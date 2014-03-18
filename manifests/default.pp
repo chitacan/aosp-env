c3abeece09013bf8d7b10180273b07dfc63d1d81
kyung yeol kim <chitacan@gmail.com>
2014-03-19 00:46:18 +0900

    add class.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,31 +1,33 @@
-case $operatingsystem {
-  ubuntu: {
-    $apache = 'apache2'
-    $configfile = '/etc/apache2/sites-enabled/000-default'
+class webserver{
+  case $operatingsystem {
+    ubuntu: {
+      $apache = 'apache2'
+      $configfile = '/etc/apache2/sites-enabled/000-default'
+    }
   }
-}
 
-package { 'apache':
-  name   => $apache,
-  ensure => 'installed',
-}
+  package { 'apache':
+    name   => $apache,
+    ensure => 'installed',
+  }
 
-file { 'site-config':
-  path    => $configfile,
-  source  => '/vagrant/manifests/site-config',
-  require => Package['apache'],
-}
+  file { 'site-config':
+    path    => $configfile,
+    source  => '/vagrant/manifests/site-config',
+    require => Package['apache'],
+  }
 
-service { 'apache':
-  name       => $apache,
-  ensure     => 'running',
-  hasrestart => true,
-  subscribe  => File['site-config']
-}
+  service { 'apache':
+    name       => $apache,
+    ensure     => 'running',
+    hasrestart => true,
+    subscribe  => File['site-config']
+  }
 
-file { '/vagrant/index.html':
-  content => "<h1> vagrant + puppet + ${apache} + ${operatingsystem}</h1>",
-}
+  file { '/vagrant/index.html':
+    content => "<h1> vagrant + puppet + ${apache} + ${operatingsystem}</h1>",
+  }
 
-if $apache == 'apache2' {
+  if $apache == 'apache2' {
+  }
 }
