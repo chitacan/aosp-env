bc7c55ee2a7610dd981cc4db2266dd443f752811
kyung yeol kim <chitacan@gmail.com>
2014-03-19 18:43:55 +0900

    implement new puppet script.
    this will install
     - python-software-properties
     - tmux
     - git
     - oracle-java6-installer

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,1 +1,32 @@
-include tmux
+exec { 'update':
+  path    => '/usr/bin',
+  command => 'apt-get update',
+}
+
+package { 'psp':
+  name   => 'python-software-properties',
+  ensure => 'installed'
+}
+
+class java {
+  class { 'apt': }
+  apt::ppa { 'ppa:webupd8team/java': } ->
+  # Prepare response file
+  file { "/tmp/oracle-java6-installer.preseed":
+    source => '/vagrant/java.response',
+    mode   => 600,
+    backup => false,
+  } ->
+  # Install Java
+  package { "oracle-java6-installer":
+    ensure       => "installed",
+    responsefile => '/tmp/oracle-java6-installer.preseed'
+  } ->
+  package { "oracle-java6-set-default": ensure => "installed" }
+}
+
+Exec['update']    ->
+Package['psp']    ->
+class { 'tmux': } ->
+class { 'git': }  ->
+class { 'java': }
